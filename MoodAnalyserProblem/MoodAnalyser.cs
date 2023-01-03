f61d75a74cd6bd493781069a6ae69245346e77a6
rishiraj007 <rishiraj@mounttalent.com>
2023-01-03 16:41:28 +0530

    [Prabhu] Add: Mood Analyser

--- MoodAnalyserProblem/MoodAnalyser.cs
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -2,6 +2,11 @@
 {
     public class MoodAnalyser
     {
-       
+        public string AnalyseMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+                return "Sad";
+            return "Happy";
+        }
     }
 }
